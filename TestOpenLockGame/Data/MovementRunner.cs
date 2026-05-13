e37d49d65059e31ad5b14b17e87b8c059a1e9a9c
matvejskripov <matvejskripov@gmail.com>
2026-05-13 16:59:26 +0700

    Добавьте файлы проекта.

---
+++ TestOpenLockGame/Data/MovementRunner.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace TestOpenLockGame.Data
+{
+    class MovementRunner
+    {
+        public static bool UpMove(Border Runner, double SpeedwayActualHeight, double Speed)
+            => Move(Runner, Speed, true, SpeedwayActualHeight);
+        public static bool DownMove(Border Runner, double Speed)
+            => Move(Runner, Speed, false);
+
+        private static bool Move(Border Runner, double Speed, bool UpOrDown, double SpeedwayActualHeight = -1)
+        {
+            double boundary = UpOrDown ?
+                SpeedwayActualHeight - (Runner.Margin.Bottom + Runner.ActualHeight) :
+                Runner.Margin.Bottom;
+
+            if (boundary > 0)
+            {
+                Runner.Margin = boundary - Speed > 0 ?
+                        new Thickness(0, 0, 0, Runner.Margin.Bottom + (Speed * (UpOrDown ? 1 : -1))) : // Увеличиваем/уменьшаем расстояние между бегунком и началом трека
+                        new Thickness(0, 0, 0, UpOrDown ? SpeedwayActualHeight - Runner.ActualHeight : 0); // Соприкасаем бегунок с концом/началом трека
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ TestOpenLockGame/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using TestOpenLockGame.Data;
+
+namespace TestOpenLockGame;
+
+public partial class MainWindow : Window
+{
+    private DispatcherTimer Timer;
+
+    private bool MoveUpOrDown = true;
+
+    private int ChoiceSpeedway = 0;
+
+    private int VictoryPoints = 0;
+
+    private int SpeedChoice = 0;   
+    private readonly int[] PercentSpeeds = [1, 2, 3, 4, 5];
+
+    private const int PercentSaveZone = 10;    
+
+
+    private double SpeedwayActualHeight() => Speedway.ActualHeight;
+    private Border ChoiceRunner() => FindName("Runner" + ChoiceSpeedway) as Border;
+    private double CheckZone() => SpeedwayActualHeight() / 100 * 30;
+    private double SaveZone() => SpeedwayActualHeight() / 100 * PercentSaveZone;
+    private double Speed(int Speed) => SpeedwayActualHeight() / 100 * Speed;
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Timer = new DispatcherTimer();
+        Timer.Interval = TimeSpan.FromMilliseconds(1);
+
+        Timer.Tick += Timer_Tick;
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        Border runner = ChoiceRunner();
+        double speed = Speed(PercentSpeeds[SpeedChoice]);
+
+        if (MoveUpOrDown)
+        {
+            if (MovementRunner.UpMove(runner, SpeedwayActualHeight(), speed))
+            {
+                MoveUpOrDown = false;
+            }
+        }
+        else
+        {
+            if (MovementRunner.DownMove(runner, speed))
+            {
+                MoveUpOrDown = true;
+                Timer.Stop();
+            }
+        }
+    }
+
+    private void Control(object sender, KeyEventArgs e)
+    {        
+        switch (e.Key.ToString())
+        {
+            case "Space":
+                Start();
+                break;
+
+            case "Left":
+                ChoiceLeft();
+                break;
+
+            case "Right":
+                ChoiceRight();
+                break;
+        }
+    }     
+
+    private void Start()
+    {
+        if (!Timer.IsEnabled)
+        {   
+            if (ChoiceRunner().Margin.Bottom != CheckZone())
+            {
+                Timer.Start();
+            }
+        }
+        else
+        {
+            Lock(ChoiceRunner());
+        }
+
+        SpeedChoice = SpeedChoice < PercentSpeeds.Length - 1 ? ++SpeedChoice : 0;
+    }
+
+    private void Lock(Border Runner)
+    {
+        Timer.Stop();
+
+        double max = Math.Abs(Runner.Margin.Bottom - CheckZone());
+
+        if (max <= SaveZone())
+        {
+            Runner.Margin = new Thickness(0, 0, 0, CheckZone());
+
+            VictoryPoints++;
+
+            if (VictoryPoints == 5)
+            {
+                MessageBox.Show("ПОБЕДА!");
+                Close();
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                (FindName("Runner" + i) as Border).Margin = new Thickness(0);
+                VictoryPoints = 0;
+            }
+        }
+    }
+
+    private void ChoiceLeft()
+    {
+        if (ChoiceSpeedway > 0 & !Timer.IsEnabled)
+        {
+            Grid.SetColumn(Selector, --ChoiceSpeedway);
+        }
+    }
+
+    private void ChoiceRight()
+    {
+        if (ChoiceSpeedway < 4 & !Timer.IsEnabled)
+        {
+            Grid.SetColumn(Selector, ++ChoiceSpeedway);
+        }
+    }
+}
